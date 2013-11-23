b5ae3cee68c3b05d4d9344d9e8a8b8315e832253
rakesh <rkchoudhary0208@gmail.com>
2013-11-24 02:00:55 +0530

    http://oj.leetcode.com/problems/maximal-rectangle/

--- src/MaximalRectangle.java
+++ src/MaximalRectangle.java
@@ -1,19 +1,49 @@
 import java.util.Stack;
 
 /*
-http://oj.leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+http://oj.leetcode.com/problems/maximal-rectangle/
  */
-class StackElement {
-	int s;
-	int val;
 
-	StackElement(int s, int val) {
-		this.s = s;
-		this.val = val;
+
+public class MaximalRectangle {
+	class StackElement {
+		int s;
+		int val;
+
+		StackElement(int s, int val) {
+			this.s = s;
+			this.val = val;
+		}
 	}
-}
 
-public class LargestRectangleArea {
+	public int maximalRectangle(char [][] src) {
+		if(src.length==0) {
+			return 0;
+		}
+		int max=0;
+		int [] heights=new int[src[0].length];
+		for(int row=0;row<src.length;row++) {
+			for(int col=0;col<src[0].length;col++) {
+				if(row==0) {
+					heights[col]=src[row][col]=='1'?1:0;
+				}else {
+					if(src[row][col]=='1') {
+						heights[col]+=1;
+					}else {
+						heights[col]=0;
+					}
+				}
+			}
+
+			int ret=largestRectangleArea(heights);
+			if(max<ret) {
+				max=ret;
+			}
+
+		}
+		return max;
+	}
+
 	public int largestRectangleArea(int[] histogram) {
 		if (histogram.length == 0) {
 			return 0;
@@ -22,7 +52,7 @@ public class LargestRectangleArea {
 		Stack<StackElement> stack = new Stack<StackElement>();
 		for (int i = 0; i < histogram.length; i++) {
 			StackElement el=null;
-		int start=i;
+			int start=i;
 			while (!stack.empty() && histogram[i] < stack.peek().val) {
 				start=stack.peek().s;
 				max = calculateMax(max, stack, i);
