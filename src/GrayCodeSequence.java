3fed9f3fe90b83ab663ce82744ffb0febc67ca27
rakesh <rkchoudhary0208@gmail.com>
2013-11-24 16:22:00 +0530

    http://oj.leetcode.com/problems/gray-code/

---
+++ src/GrayCodeSequence.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+/*
+http://oj.leetcode.com/problems/gray-code/
+ */
+public class GrayCodeSequence {
+	public ArrayList<Integer> grayCode(int n) {
+		if(n==0) {
+			ArrayList<Integer> ret= new ArrayList<Integer>();
+			ret.add(0);
+			return ret;
+
+		}
+
+
+		return grayCodeUtil(n);
+	}
+
+	private ArrayList<Integer> grayCodeUtil(int n) {
+		ArrayList<Integer> ret=new ArrayList<Integer>();
+		if(n==1) {
+			ret.add(1);
+			ret.add(0);
+			return ret;
+		}
+
+		ArrayList<Integer> gray=new ArrayList<Integer>();
+		for(Integer el:grayCodeUtil(--n)) {
+			ret.add(new Integer(el));
+			gray.add(0,new Integer((1<<(n-1))+el));
+		}
+		ret.addAll(gray);
+		return ret;
+	}
+}
