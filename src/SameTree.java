f553a3d4cd8b1a49769f7d691dda38ff1bb1b066
rakesh <rkchoudhary0208@gmail.com>
2013-11-25 22:55:38 +0530

    http://oj.leetcode.com/problems/same-tree/

---
+++ src/SameTree.java
@@ -0,0 +1,20 @@
+/*
+http://oj.leetcode.com/problems/same-tree/
+ */
+public class SameTree {
+	public boolean isSameTree(TreeNode p,TreeNode q) {
+		if(p==null&& q==null) {
+			return true;
+		}
+		if(p==null||q==null) {
+			return false;
+		}
+
+		if(p.val!=q.val)
+		{
+			return false;
+		}
+		return isSameTree(p.left,q.left)&& isSameTree(p.right,q.right);
+
+	}
+}
