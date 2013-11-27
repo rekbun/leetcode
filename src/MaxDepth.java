887114f5a1108a1ef0fd1ad4dbf0c0c82800f99d
rakesh <rkchoudhary0208@gmail.com>
2013-11-27 23:58:43 +0530

    http://oj.leetcode.com/problems/maximum-depth-of-binary-tree/

---
+++ src/MaxDepth.java
@@ -0,0 +1,13 @@
+/*
+http://oj.leetcode.com/problems/maximum-depth-of-binary-tree/
+ */
+public class MaxDepth {
+	public int maxDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		int left = maxDepth(root.left);
+		int right = maxDepth(root.right);
+		return left < right ? right + 1 : left + 1;
+	}
+}
