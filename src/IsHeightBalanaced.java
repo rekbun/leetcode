864de05d625e6d9573f86fffe95a966a04e1dd1d
rakesh <rkchoudhary0208@gmail.com>
2013-11-29 11:35:25 +0530

    http://oj.leetcode.com/problems/balanced-binary-tree/

--- src/IsHeightBalanaced.java
+++ src/IsHeightBalanaced.java
@@ -2,14 +2,14 @@
 
  */
 public class IsHeightBalanaced {
-	public boolean isHeightBalanced(TreeNode root) {
+	public boolean isBalanced(TreeNode root) {
 		if(root==null) {
 			return true;
 		}
 
 		BalanceH bh=new BalanceH();
-		int h= isHeightBalancedUtil(root,bh);
-		if(h<=1 && h>=-1) {
+		isHeightBalancedUtil(root,bh);
+		if(bh.d<=1 && bh.d>=-1) {
 			return true;
 		}
 		return false;
@@ -28,11 +28,9 @@ public class IsHeightBalanaced {
 		int r=isHeightBalancedUtil(root.right,bh);
 
 		int d=l-r;
-		if(d>1 && d<-1) {
+		if(d>1 || d<-1) {
 			bh.d=d;
 		}
-		return l>r?l : r;
+		return l>r?l+1 : r+1;
 	}
-
-
 }
