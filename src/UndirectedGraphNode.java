6950224096f3745f73d42c0a71827a4ec2e3db3e
rakesh <rkchoudhary0208@gmail.com>
2013-12-11 01:38:25 +0530

    http://oj.leetcode.com/problems/clone-graph/

---
+++ src/UndirectedGraphNode.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class UndirectedGraphNode {
+    int label;
+    ArrayList<UndirectedGraphNode> neighbors;
+    UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+}
