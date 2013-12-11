ac00ad648203e9d6685cf8fd1ddd2d05d6137c7d
rakesh <rkchoudhary0208@gmail.com>
2013-12-11 17:31:21 +0530

    http://oj.leetcode.com/problems/single-number/

---
+++ src/SingleNumber.java
@@ -0,0 +1,13 @@
+/*
+http://oj.leetcode.com/problems/single-number/
+ */
+public class SingleNumber {
+	public int singleNumber(int[] A) {
+		int el=0;
+		for(int i=0;i<A.length;i++) {
+			el=el^A[i];
+		}
+
+		return el;
+	}
+}
