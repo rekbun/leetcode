ed266a178822900d185999c876cbec87d641c868
rakesh <rkchoudhary0208@gmail.com>
2013-12-12 01:04:43 +0530

    http://oj.leetcode.com/problems/linked-list-cycle/

---
+++ src/LinkedListCycle.java
@@ -0,0 +1,22 @@
+/*
+http://oj.leetcode.com/problems/linked-list-cycle/
+ */
+public class LinkedListCycle {
+	public boolean hasCycle(ListNode head) {
+		if(head==null) {
+			return false;
+		}
+		ListNode slow=head;
+		ListNode fast=head.next;
+
+		while(slow!=fast && (fast!=null && fast.next!=null)) {
+			slow=slow.next;
+			fast=fast.next.next;
+		}
+
+		if(slow==fast && fast!=null && fast.next!=null) {
+			return true;
+		}
+		return false;
+	}
+}
