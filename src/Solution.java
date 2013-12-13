187ae4b98d45b8fc55866f3823ad408ca6d7878e
rakesh <rkchoudhary0208@gmail.com>
2013-12-14 01:11:40 +0530

    solution added

--- src/Solution.java
+++ src/Solution.java
@@ -3,13 +3,13 @@ import java.util.HashSet;
 
 public class Solution {
 	public static void main(String[] args) {
-		WordLadderII sol=new WordLadderII();
-		HashSet<String> set =new HashSet<String>();
-		set.add("a");
-		set.add("b");
-		set.add("c");
-		sol.findLadders("a","c",set);
-
+		WordBreakII sol=new WordBreakII();
+		HashSet<String> set=new HashSet<String>();
+		set.add("cat");
+		set.add("cats");
+		set.add("and");
+		set.add("sand");
+		set.add("dog");
+		sol.wordBreak("catsanddog",set);
 	}
-
 }
