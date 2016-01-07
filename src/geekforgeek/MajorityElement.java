a3a7c7382792a007103e9c422d284882f04b0e47
rakesh kumar <rkchoudhary0208@gmail.com>
2016-01-07 21:22:22 +0530

    adds majority element

---
+++ src/geekforgeek/MajorityElement.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.io.BufferedReader;
+public class MajorityElement {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int nS=Integer.parseInt(reader.readLine());
+		int[] array=Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		int i=1;
+		int cnt=1;
+
+		while(i<array.length) {
+			if(array[i]==array[0]) {
+				cnt++;
+			}
+			i++;
+		}
+
+		if(cnt==array.length/2) {
+			System.out.println(cnt);
+		}
+
+		findMajority(array);		
+	}
+
+	public static int findMajority(int[] array) {
+
+		int c=1;
+		int v=array[1];
+		int s=2;
+
+		while(s<array.length) {
+			if(array[s]==v) {
+				c++;
+			}
+			else {
+				c--;
+			}
+			if(c==0) {
+				v=array[s];
+				c=1;
+			}
+		}
+		return v;
+		
+	}
+}
